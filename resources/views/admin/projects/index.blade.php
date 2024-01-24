cf799694bb12874f6f0f3abd7a50ecebbb83f1a1
MadalinaIndreica <madalinaindreicamaddy4@gmail.com>
2024-01-24 14:30:02 +0100

    add: destroy operation

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -5,17 +5,23 @@
         <h2>Lista dei progetti</h2>
 
         <div class="text-end">
-          <a class="btn btn-info" href="{{ route('admin.projects.create') }}">Crea un nuovo progetto</a>
+            <a class="btn btn-info" href="{{ route('admin.projects.create') }}">Crea un nuovo progetto</a>
         </div>
+        @if (session('message'))
+            <div class="alert alert-success">
+              {{ session('message') }}
+            </div>
+        @endif
 
+        @if (count($projects) > 0)
         <table class="table table-striped mt-5">
             <thead>
-              <tr>
-                <th scope="col">id</th>
-                <th scope="col">Titolo</th>
-                <th scope="col">Data</th>
-                <th scope="col">Actions</th>
-              </tr>
+                <tr>
+                    <th scope="col">id</th>
+                    <th scope="col">Titolo</th>
+                    <th scope="col">Data</th>
+                    <th scope="col">Actions</th>
+                </tr>
             </thead>
             <tbody>
                 @foreach ($projects as $project)
@@ -24,12 +30,26 @@
                         <td>{{ $project->title }}</td>
                         <td>{{ $project->created_at }}</td>
                         <td>
-                          <a class="btn btn-primary" href="{{ route('admin.projects.show', ['project' => $project->slug]) }}">Dettagli</a>
-                          <a class="btn btn-warning" href="{{ route('admin.projects.edit', ['project' => $project->slug]) }}">Modifica</a>
+                            <a class="btn btn-primary"
+                                href="{{ route('admin.projects.show', ['project' => $project->slug]) }}">Dettagli</a>
+                            <a class="btn btn-warning"
+                                href="{{ route('admin.projects.edit', ['project' => $project->slug]) }}">Modifica</a>
+                            <form class="d-inline-block"
+                                action="{{ route('admin.projects.destroy', ['project' => $project->slug]) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">Cancella</button>
+                            </form>
                         </td>
                     </tr>
                 @endforeach
             </tbody>
-          </table>
+        </table>
+        @else
+        <div class="alert alert-warning mt-5">
+          Il portfolio è vuoto, non c'è ancora nessun progetto!
+        </div>
+            
+        @endif
     </div>
 @endsection
